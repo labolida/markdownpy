39bc3eebb4a4ca20511e4950b354bb1e58a43cd1
Leonardo Labolida <l.labolida@GE.BIZ>
2023-06-01 13:15:35 +0200

    adding header and footer as a resources

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ app = Flask(__name__)
 ##########################################################################################
 # CONFIG
 
+#DATA_PATH = "/disk01/repositories/markdownpy-data/"
 DATA_PATH = r'C:\\area\\dev\\markdownpy\\data\\'
+RESOURCES_PATH = 'C:\\area\\dev\\markdownpy\\resources\\'
 
 ##########################################################################################
 @app.route('/')
@@ -28,14 +30,20 @@ def root():
 ##########################################################################################
 @app.route('/view')
 def view():
-    with open( DATA_PATH+"index.md", "r", encoding="utf-8") as input_file:
-        text = input_file.read()
-    html = markdown.markdown(text)
-    print("\n\n\n\n\n----------------------------------------------------\n\n\n\n")
+    html = ""
+    html = html + fileRead( RESOURCES_PATH+"header.html")
+    markdown_content = fileRead( DATA_PATH+"index.md")
+    html = html + markdown.markdown(markdown_content)
+    html = html + fileRead( RESOURCES_PATH+"footer.html")
+    print("\n\n----------------------------------------------------\n\n")
     print(html)
-    html = "<html> css include " + html
     return html
 
 
 
+##########################################################################################
+def fileRead( filename ):
+    with open( filename, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+    return text
 
